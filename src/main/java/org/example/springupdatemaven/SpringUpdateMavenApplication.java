b70e14b14f2758d381e5c65735b841e56e62f599
surya <contactsurya2@gmail.com>
2024-09-10 14:00:07 +0530

    Spring 2.6.2

---
+++ src/main/java/org/example/springupdatemaven/SpringUpdateMavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.springupdatemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUpdateMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUpdateMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springupdatemaven/SpringUpdateMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springupdatemaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUpdateMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
